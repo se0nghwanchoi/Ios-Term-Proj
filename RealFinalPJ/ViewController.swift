eb3c3be30824a1c2b0280b71fbab229016f7cee3
se0nghwanchoi <a01036459946@gmail.com>
2024-06-19 05:31:33 +0900

    Initial Commit

---
+++ RealFinalPJ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealFinalPJ
+//
+//  Created by csh on 2024/06/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
