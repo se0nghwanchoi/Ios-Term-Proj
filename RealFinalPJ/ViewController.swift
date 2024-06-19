59765215c1337815752ec6c13c43bfd712dd3c26
se0nghwanchoi <a01036459946@gmail.com>
2024-06-19 16:49:28 +0900

    add view

--- RealFinalPJ/ViewController.swift
+++ RealFinalPJ/ViewController.swift
@@ -9,87 +9,99 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var mainLabel: UILabel!
-    @IBOutlet weak var heigthTextField: UITextField!
-    @IBOutlet weak var weightTextField: UITextField!
-    @IBOutlet weak var calcButton: UIButton!
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var heightInput: UITextField!
+    @IBOutlet weak var weightInput: UITextField!
+    @IBOutlet weak var resultButton: UIButton!
+    
+    
+    // BMI 계산기 관리자 인스턴스
+    var bmiManager = BMICalculatorManager()
+        
+    // BMI 데이터를 저장할 변수
+    var bmi: Double?
+    var bmiToString: String?
+    var bmiColor: UIColor?
+
+    // 뷰가 로드될 때 호출되는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureUI()
-    }
-    
-    var bmiManager = BMICalculatorManager()
-    
-    var bmi: Double?
-    var adviceString: String?
-    var bmiColor: UIColor?
-    
-    func configureUI() {
-        heigthTextField.delegate = self
-        weightTextField.delegate = self
+        // 초기 UI 설정
+        SettingUI()
+        }
         
-        view.backgroundColor = #colorLiteral(red: 0.9321654319, green: 0.9252404467, blue: 0.7937848456, alpha: 1)
-        mainLabel.text = "키와 몸무게를 입력해 주세요"
-        calcButton.clipsToBounds = true
-        calcButton.layer.cornerRadius = 5
-        calcButton.setTitle("BMI 계산하기", for: .normal)
-        heigthTextField.placeholder = "cm 단위로 입력해주세요"
-        weightTextField.placeholder = "kg 단위로 입력해주세요"
-    }
+    // UI를 초기 설정하는 함수
+    func SettingUI() {
+        heightInput.delegate = self
+        weightInput.delegate = self
+            
+        
+        firstLabel.text = "키와 체중을 입력하세요"
+        resultButton.clipsToBounds = true
+        resultButton.layer.cornerRadius = 5
+        resultButton.setTitle("계산하기", for: .normal)
+        heightInput.placeholder = "키를 입력하세요(cm)"
+        weightInput.placeholder = "체중을 입력하세요(kg)"
+        }
     
-    @IBAction func calcButtonTapped(_ sender: UIButton) {
+    @IBAction func resultButtonTapped(_ sender: UIButton) {
         
     }
     
+    // segue 수행 전 입력 값을 검증하는 함수
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        if heigthTextField.text == "" || weightTextField.text == "" {
-            mainLabel.text = "키와 몸무게 입력이 필요합니다."
-            mainLabel.textColor = .red
-            return false
-        }
-        mainLabel.text = "키와 몸무게를 입력해 주세요"
-        mainLabel.textColor = .black
-        return true
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "toResultVC" {
-            let resultVC = segue.destination as! ResultViewController
-            
-            guard let w = weightTextField.text,
-                  let h = heigthTextField.text else {return}
-            
-            resultVC.bmi = bmiManager.getBMI(height: h, weight: w)
-        }
-        
-        heigthTextField.text = ""
-        weightTextField.text = ""
-    }
-}
-
-extension ViewController: UITextFieldDelegate {
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        if Int(string) != nil {
+            if heightInput.text == "" || weightInput.text == "" {
+                firstLabel.text = "키와 체중을 입력해주세요"
+                firstLabel.textColor = .green
+                return false
+            }
+            firstLabel.text = "키와 체중을 입력해주세요"
+            firstLabel.textColor = .darkGray
             return true
         }
-        return false
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if heigthTextField.text != "", weightTextField.text != "" {
-            weightTextField.resignFirstResponder()
-        } else if heigthTextField.text != "" {
-            weightTextField.becomeFirstResponder()
+        
+    // segue 수행 전에 데이터를 준비하는 함수
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "toResultVC" {
+                let resultVC = segue.destination as! SecondViewController
+                
+                guard let weightT = weightInput.text, let heightT = heightInput.text else {
+                    return
+                }
+                
+                resultVC.bmi = bmiManager.getResult(height: heightT, weight: weightT)
+            }
+            
+            // 입력 필드를 초기화
+            heightInput.text = ""
+            weightInput.text = ""
         }
-        return false
     }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        heigthTextField.resignFirstResponder()
-        weightTextField.resignFirstResponder()
-    }
+
+    // UITextFieldDelegate 확장을 통해 텍스트 필드의 동작을 처리
+    extension ViewController: UITextFieldDelegate {
+        
+        // 숫자 입력만 허용하도록 설정하는 함수
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            return Int(string) != nil
+        }
+        
+        // 리턴 키 동작을 처리하는 함수
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            if heightInput.text != "", weightInput.text != "" {
+                weightInput.resignFirstResponder()
+            } else if heightInput.text != "" {
+                weightInput.becomeFirstResponder()
+            }
+            return false
+        }
+        
+        // 텍스트 필드 외부를 터치했을 때 키보드를 숨기는 함수
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            heightInput.resignFirstResponder()
+            weightInput.resignFirstResponder()
+        }
 
 }
 
